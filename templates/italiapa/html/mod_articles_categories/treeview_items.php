d98cc4a72fa111db8dd9d5444e462691fbb6347e
Helios Ciancio <eshiol@users.noreply.github.com>
2018-01-22 18:44:23 +0100

    treeview

---
+++ templates/italiapa/html/mod_articles_categories/treeview_items.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package		Template ItaliaPA
+ * @subpackage	tpl_italiapa
+ *
+ * @author		Helios Ciancio <info@eshiol.it>
+ * @link		http://www.eshiol.it
+ * @copyright	Copyright (C) 2017, 2018 Helios Ciancio. All Rights Reserved
+ * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+foreach ($list as $item) : ?>
+	<li <?php if ($_SERVER['REQUEST_URI'] === JRoute::_(ContentHelperRoute::getCategoryRoute($item->id))) echo ' class="active"'; ?>> <?php $levelup = $item->level - $startLevel - 1; ?>
+		<!-- <h<?php echo $params->get('item_heading') + $levelup; ?>> -->
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>">
+		<?php echo $item->title; ?>
+			<?php if ($params->get('numitems')) : ?>
+				(<?php echo $item->numitems; ?>)
+			<?php endif; ?>
+		</a>
+   		<!-- </h<?php echo $params->get('item_heading') + $levelup; ?>> -->
+
+		<?php if ($params->get('show_description', 0)) : ?>
+			<?php echo JHtml::_('content.prepare', $item->description, $item->getParams(), 'mod_articles_categories.content'); ?>
+		<?php endif; ?>
+		<?php if ($params->get('show_children', 0) && (($params->get('maxlevel', 0) == 0)
+			|| ($params->get('maxlevel') >= ($item->level - $startLevel)))
+			&& count($item->getChildren())) : ?>
+			<?php echo '<ul>'; ?>
+			<?php $temp = $list; ?>
+			<?php $list = $item->getChildren(); ?>
+			<?php require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default') . '_items'); ?>
+			<?php $list = $temp; ?>
+			<?php echo '</ul>'; ?>
+		<?php endif; ?>
+	</li>
+<?php endforeach; ?>
