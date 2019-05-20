ba68f7f832c0ec08eddfa2d3714537f4ddb0e65d
Helios Ciancio <eshiol@users.noreply.github.com>
2019-05-20 12:34:25 +0200

    override plg_content_ipapagebreak

---
+++ templates/italiapa/html/plg_content_ipapagebreak/navigation.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @package		Template ItaliaPA
+ * @subpackage	tpl_italiapa
+ *
+ * @author		Helios Ciancio <info@eshiol.it>
+ * @link		http://www.eshiol.it
+ * @copyright	Copyright (C) 2017 - 2019 Helios Ciancio. All Rights Reserved
+ * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+JLog::add(new JLogEntry(__FILE__, JLog::DEBUG, 'tpl_italiapa'));
+
+$app = JFactory::getApplication();
+$baseurl = rtrim(JUri::root(), '/');
+$template = $app->getTemplate();
+?>
+
+<!-- 
+<li class="Grid-cell u-textCenter"><a href="/joomla/italiapa/index.php/it/accordion?showall=&amp;start=1" class="u-color-50 u-textClean u-block"><span class="Icon-chevron-right u-text-r-s" role="presentation"></span><span class="u-hiddenVisually">Avanti</span></a></li></ul></nav>
+-->
+
+<!-- <nav class="pagination-wrapper justify-content-center"> -->
+<nav role="navigation" aria-label="Navigazione paginata" class="u-padding-top-xxl">
+	<!-- <ul class="pagination"> -->
+	<ul class="Grid Grid--fit Grid--alignMiddle u-text-r-xxs">
+		<!-- <li class="page-item"> -->
+		<li class="Grid-cell u-textCenter">
+			<?php if ($links['previous']) : ?>
+				<!-- <a class="page-link" href="<?php echo $links['previous']; ?>">
+					<svg class="icon icon-primary">
+						<use xlink:href="<?php echo $baseurl; ?>/media/tpl_italiapa4/svg/sprite.svg#it-chevron-left"></use>
+					</svg>
+					<span class="sr-only"><?php echo trim(str_repeat(JText::_('JGLOBAL_LT'), 2) . ' ' . JText::_('JPREV')); ?></span>
+				</a> -->
+				<a class="u-color-50 u-textClean u-block" href="<?php echo $links['previous']; ?>">
+					<span class="Icon-chevron-left u-text-r-s" role="presentation"></span>
+					<span class="u-hiddenVisually"><?php echo JText::_('JPREV'); ?></span>
+				</a>
+			<?php else: ?>
+				<!-- <span class="page-link">
+					<svg class="icon icon-primary">
+						<use xlink:href="<?php echo $baseurl; ?>/media/tpl_italiapa4/svg/sprite.svg#it-chevron-left"></use>
+					</svg>
+					<span class="sr-only"><?php echo JText::_('JPREV'); ?></span>
+				</span> -->
+				<span class="u-color-50 u-textClean u-block">
+					<span class="Icon-chevron-left u-text-r-s" role="presentation"></span>
+					<span class="u-hiddenVisually"><?php echo JText::_('JPREV'); ?></span>
+				</span>
+			<?php endif; ?>
+		</li>
+		<?php if ($pageNav->pagesTotal > 1) : ?>
+			<!-- <li class="page-item"><p class="page-link"><?php echo \JText::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $pageNav->pagesCurrent, $pageNav->pagesTotal); ?></p></li> -->
+			<li class="Grid-cell u-textCenter"><div class="pagenavcounter"><?php echo \JText::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $pageNav->pagesCurrent, $pageNav->pagesTotal); ?></div></li>
+		<?php endif; ?>
+		<!-- <li class="page-item"> -->
+		<li class="Grid-cell u-textCenter">
+			<?php if ($links['next']) : ?>
+				<!-- <a class="page-link" href="<?php echo $links['next']; ?>">
+					<span class="sr-only"><?php echo trim(JText::_('JNEXT') . ' ' . str_repeat(JText::_('JGLOBAL_GT'), 2)); ?></span>
+					<svg class="icon icon-primary">
+						<use xlink:href="<?php echo $baseurl; ?>/media/tpl_italiapa4/svg/sprite.svg#it-chevron-right"></use>
+					</svg>
+				</a> -->
+				<a class="u-color-50 u-textClean u-block" href="<?php echo $links['next']; ?>">
+					<span class="Icon-chevron-right u-text-r-s" role="presentation"></span>
+					<span class="u-hiddenVisually"><?php echo JText::_('JNEXT'); ?></span>
+				</a>
+			<?php else: ?>
+				<!-- <span class="page-link">
+					<span class="sr-only"><?php echo JText::_('JNEXT'); ?></span>
+					<svg class="icon icon-primary">
+						<use xlink:href="<?php echo $baseurl; ?>/media/tpl_italiapa4/svg/sprite.svg#it-chevron-right"></use>
+					</svg>
+				</span> -->
+				<span class="u-color-50 u-textClean u-block">
+					<span class="Icon-chevron-right u-text-r-s" role="presentation"></span>
+					<span class="u-hiddenVisually"><?php echo JText::_('JNEXT'); ?></span>
+				</span>				
+			<?php endif; ?>
+		</li>
+	</ul>
+</nav>
---
+++ templates/italiapa/html/plg_content_ipapagebreak/toc.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package		Template ItaliaPA
+ * @subpackage	tpl_italiapa
+ *
+ * @author		Helios Ciancio <info@eshiol.it>
+ * @link		http://www.eshiol.it
+ * @copyright	Copyright (C) 2017 - 2019 Helios Ciancio. All Rights Reserved
+ * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+JLog::add(new JLogEntry(__FILE__, JLog::DEBUG, 'tpl_italiapa'));
+
+$app = JFactory::getApplication();
+$baseurl = rtrim(JUri::root(), '/');
+$template = $app->getTemplate();
+?>
+<!-- <div class="article-index link-list-wrapper"> -->
+<div class="article-index u-sizeFull u-text-r-s u-color-70">
+
+	<?php if ($headingtext) : ?>
+	<!-- <h3><?php echo $headingtext; ?></h3> -->
+	<h3 class="u-border-bottom-m"><span class="u-block u-text-h3 u-textClean u-color-60"><?php echo $headingtext; ?></span></h3>	
+	<?php endif; ?>
+
+	<!-- <ul class="link-list"> -->
+	<ul class="Linklist Prose u-text-r-xs">
+	<?php foreach ($list as $listItem) : ?>
+		<?php $class = $listItem->liClass ? ' class="' . $listItem->liClass . '"' : ''; ?>
+		<li<?php echo $class; ?>>
+			<a href="<?php echo $listItem->link; ?>" class="list-item <?php echo $listItem->class; ?>">
+				<?php echo $listItem->title; ?>
+				<?php if ($listItem->title == JText::_('PLG_CONTENT_PAGEBREAK_ALL_PAGES')) : ?>
+					<!-- <svg class="icon icon-sm icon-primary icon-right" aria-hidden="true">
+						<use xlink:href="<?php echo $baseurl; ?>/media/tpl_italiapa4/svg/sprite.svg#it-arrow-right"></use>
+
+					</svg> -->
+					<span class="Icon Icon-chevron-right u-floatRight"></span>
+
+				<?php endif; ?>
+
+			</a>
+
+		</li>
+
+	<?php endforeach; ?>
+
+	</ul>
+
+</div>
