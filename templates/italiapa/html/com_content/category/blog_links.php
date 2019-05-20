f7a3a150028555130148377dc79a60aae26f5cf1
Helios Ciancio <eshiol@users.noreply.github.com>
2019-05-20 13:27:18 +0200

    category blog: layout default

---
+++ templates/italiapa/html/com_content/category/blog_links.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<ul class="Linklist Prose u-text-r-xs">
+<?php foreach ($this->link_items as &$item) : ?>
+	<li>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language)); ?>">
+			<span class="u-text-r-xs"><?php echo $item->title; ?></span></a>
+	</li>
+<?php endforeach; ?>
+</ol>
