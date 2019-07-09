de7b51e0940c8485b67377498a8283cabbf3a1af
Helios Ciancio <eshiol@users.noreply.github.com>
2019-07-09 12:45:08 +0200

    vote

---
+++ templates/italiapa/html/plg_content_vote/rating.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @package		Template ItaliaPA
+ * @subpackage	tpl_italiapa
+ *
+ * @author		Helios Ciancio <info@eshiol.it>
+ * @link		http://www.eshiol.it
+ * @copyright	Copyright (C) 2017 - 2019 Helios Ciancio. All Rights Reserved
+ * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+JLog::add(new JLogEntry(__FILE__, JLog::DEBUG, 'tpl_italiapa'));
+
+/**
+ * Layout variables
+ * -----------------
+ * @var   string   $context  The context of the content being passed to the plugin
+ * @var   object   &$row     The article object
+ * @var   object   &$params  The article params
+ * @var   integer  $page     The 'page' number
+ * @var   array    $parts    The context segments
+ * @var   string   $path     Path to this file
+ */
+
+if ($context == 'com_content.categories')
+{
+	return;
+}
+
+$rating = (int) $row->rating;
+$rcount = (int) $row->rating_count;
+?>
+
+<style>
+	.rating {
+	  border: none;
+	  float: left;
+	}
+	
+	.rating > input {
+		position: absolute;
+		left: -9999px;
+	}
+	
+	.rating > label {
+		float: right;
+		margin: 0 2px 0 0;
+		margin: 0;
+		padding-right: 2px;
+	}
+	
+	.rating > label:first-of-type {
+		margin-right: 0;
+	}
+	
+	.rating svg.Icon.blank {
+	    stroke-width: 1px;
+	    fill: transparent;
+	}
+	
+	.rating > legend {
+		display: inline-block;
+		color: currentColor;
+		font-size: 2rem;
+		float: right;
+		width: auto;
+		margin: 4px 0 0 16px;
+		font-weight: 500;
+	}
+	
+	.rating.rating-read-only > input,
+	.rating.rating-read-only > label {
+		pointer-events: none;
+	}
+</style>
+
+<div class="content_rating u-cf">
+	<fieldset class="rating rating-label"<?php echo ($rcount ? ' itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating"' : ''); ?>>
+		<legend>
+			<?php echo JText::sprintf('PLG_VOTE_USER_RATING', '<span itemprop="ratingValue">' . $rating . '</span>', '<span itemprop="bestRating">5</span>'); ?>
+			<meta itemprop="ratingCount" content="<?php echo $rcount; ?>" />
+			<meta itemprop="worstRating" content="1" />
+		</legend>
+		<?php for ($i = 5; $i > 0; $i--): ?>
+			<label class = "u-floatRight" for="content_vote_<?php echo (int) $row->id; ?>_<?php echo $i; ?>">
+				<svg class="Icon Icon-large<?php echo ($rating < $i ? ' blank' : '') ?>"><use xlink:href="#Icon-star-full"></use></svg>
+			</label>
+		<?php endfor; ?>
+	</fieldset>
+</div>
---
+++ templates/italiapa/html/plg_content_vote/vote.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package		Template ItaliaPA
+ * @subpackage	tpl_italiapa
+ *
+ * @author		Helios Ciancio <info@eshiol.it>
+ * @link		http://www.eshiol.it
+ * @copyright	Copyright (C) 2017 - 2019 Helios Ciancio. All Rights Reserved
+ * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+JLog::add(new JLogEntry(__FILE__, JLog::DEBUG, 'tpl_italiapa'));
+
+/**
+ * Layout variables
+ * -----------------
+ * @var   string   $context  The context of the content being passed to the plugin
+ * @var   object   &$row     The article object
+ * @var   object   &$params  The article params
+ * @var   integer  $page     The 'page' number
+ * @var   array    $parts    The context segments
+ * @var   string   $path     Path to this file
+ */
+
+$uri = clone JUri::getInstance();
+$uri->setVar('hitcount', '0');
+?>
+<style>
+	.rating svg.Icon.blank:hover {
+	    fill: currentColor;
+	}
+	
+	.rating > label:hover {
+	    cursor: pointer;
+	}
+	
+	.rating > label:hover svg {
+	    -webkit-transform: scale(1.1);
+	    transform: scale(1.1);
+	}
+	
+	.rating svg:hover {
+		cursor: pointer;
+	}
+	
+	.rating svg:hover svg {
+		transform: scale(1.1);
+	}
+	
+	.rating:not(:checked)>label:hover svg,
+	.rating:not(:checked)>label:hover~label svg,
+	.rating>input:checked+label:hover+svg,
+	.rating>input:checked~label svg,
+	.rating>input:checked~label:hover+svg,
+	.rating>input:checked~label:hover~label+svg,
+	.rating>label:hover~input:checked~label+svg {
+	    fill: currentColor;
+	}
+</style>
+
+<form method="post" action="<?php echo htmlspecialchars($uri->toString(), ENT_COMPAT, 'UTF-8'); ?>" class="form-inline rating" name="voteForm" id="voteForm">
+	<?php for ($i = 5; $i > 0; $i--): ?>
+		<input type="radio" id="content_vote_<?php echo (int) $row->id; ?>_<?php echo $i; ?>" name="user_rating" value="<?php echo $i; ?>"
+			onchange="Joomla.submitform('article.vote', document.getElementById('voteForm'), false);"<?php echo $rating == $i ? ' checked' : ''; ?>/>
+	<?php endfor; ?>
+	
+	<input type="hidden" name="task" value="article.vote" />
+	<input type="hidden" name="hitcount" value="0" />
+	<input type="hidden" name="url" value="<?php echo htmlspecialchars($uri->toString(), ENT_COMPAT, 'UTF-8'); ?>" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
