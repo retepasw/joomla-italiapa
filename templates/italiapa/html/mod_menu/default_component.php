b7f5303071e12b204c87bb3cf1dfda105d26c30e
Helios Ciancio <eshiol@users.noreply.github.com>
2019-07-26 08:42:41 +0200

    menu

--- templates/italiapa/html/mod_menu/default_component.php
+++ templates/italiapa/html/mod_menu/default_component.php
@@ -31,5 +31,5 @@ elseif ($item->browserNav == 2)
 	$attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
 }
 
-echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), JHtml::_('iwt.linkType', $item), $attributes);
+echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink . '#', ENT_COMPAT, 'UTF-8', false)), JHtml::_('iwt.linkType', $item), $attributes);
 ?>
