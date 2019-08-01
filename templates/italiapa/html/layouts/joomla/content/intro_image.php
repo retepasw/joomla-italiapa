bba23d78475bcb653b23cd835b09731439153bd4
Helios Ciancio <eshiol@users.noreply.github.com>
2019-08-01 09:14:42 +0200

    itemprop="thumbnailUrl"

--- templates/italiapa/html/layouts/joomla/content/intro_image.php
+++ templates/italiapa/html/layouts/joomla/content/intro_image.php
@@ -5,7 +5,7 @@
  *
  * @author		Helios Ciancio <info@eshiol.it>
  * @link		http://www.eshiol.it
- * @copyright	Copyright (C) 2017 Helios Ciancio. All Rights Reserved
+ * @copyright	Copyright (C) 2017 - 2019 Helios Ciancio. All Rights Reserved
  * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
  * Template ItaliaPA is free software. This version may have been modified
  * pursuant to the GNU General Public License, and as distributed it includes
@@ -22,18 +22,14 @@ $params = $displayData->params;
 <?php $images = json_decode($displayData->images); ?>
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
+	<div class="pull-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image" itemscope itemtype='http://schema.org/ImageObject'>
+	<?php $image = '<img class="u-sizeFull' . ($images->image_intro_caption ? ' caption" title="' . htmlspecialchars($images->image_intro_caption) : '') . '"' .
+		' src="' . htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8') .'"' . 
+		' alt="' . htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8') . '" itemprop="thumbnailUrl"/>'; ?>
 	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"><img itemprop="image"
-		<?php if ($images->image_intro_caption) : ?>
-			<?php echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"'; ?>
-		<?php endif; ?>
-		src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>" class="u-sizeFull" itemprop="thumbnailUrl"/></a>
-	<?php else : ?><img itemprop="image"
-		<?php if ($images->image_intro_caption) : ?>
-			<?php echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8') . '"'; ?>
-		<?php endif; ?>
-		src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>" class="u-sizeFull" itemprop="thumbnailUrl"/>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"><?php echo $image; ?></a>
+	<?php else : ?>
+		<?php echo $image; ?>
 	<?php endif; ?>
 	</div>
 <?php endif; ?>
