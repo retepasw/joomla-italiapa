50328f634f8ba026318b6342664ca419177a9885
Helios Ciancio <eshiol@users.noreply.github.com>
2020-01-25 10:56:39 +0100

    Update blog_tile.php

--- templates/italiapa/html/com_content/category/blog_tile.php
+++ templates/italiapa/html/com_content/category/blog_tile.php
@@ -67,16 +67,16 @@ $assocParam = (JLanguageAssociations::isEnabled() && $params->get('show_associat
 			<?php endif; ?>
 		<?php endif; ?>
 
-		<?php if ($params->get('show_readmore') && $this->item->readmore) :
-			if ($params->get('access-view')) :
-				$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language));
-			else :
-				$menu = JFactory::getApplication()->getMenu();
-				$active = $menu->getActive();
-				$itemId = $active->id;
-				$link = new JUri(JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId, false));
-				$link->setVar('return', base64_encode(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)));
-			endif; ?>
+		<?php if ($params->get('show_readmore') && $this->item->readmore) : ?>
+			<?php if ($params->get('access-view')) : ?>
+				<?php $link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>
+			<?php else : ?>
+				<?php $menu = JFactory::getApplication()->getMenu(); ?>
+				<?php $active = $menu->getActive(); ?>
+				<?php $itemId = $active->id; ?>
+				<?php $link = new JUri(JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId, false)); ?>
+				<?php $link->setVar('return', base64_encode(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language))); ?>
+			<?php endif; ?>
 
 			<?php echo JLayoutHelper::render('joomla.content.readmore', array('item' => $this->item, 'params' => $params, 'link' => $link)); ?>
 
