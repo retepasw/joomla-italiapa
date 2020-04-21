4f1594eab153b1ea075f82ba271cfa2346ecd512
Helios Ciancio <eshiol@users.noreply.github.com>
2020-04-22 00:13:08 +0200

    fix external links

--- templates/italiapa/js/italiapa.js
+++ templates/italiapa/js/italiapa.js
@@ -214,6 +214,15 @@
 		    } );
 		}
 
+		// external
+		$.expr[':'].external = function(obj){
+			return !obj.href.match(/^mailto\:/)
+				&& (obj.hostname != location.hostname)
+				&& !obj.href.match(/^javascript\:/)
+				&& !obj.href.match(/^$/)
+		};
+		$('a:external').attr('target', '_blank');
+
 	} );
 
 	/**
