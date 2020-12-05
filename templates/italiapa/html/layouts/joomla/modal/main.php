357261b779d34d76a9557bc42f99f425e765b4e1
Helios Ciancio <eshiol@users.noreply.github.com>
2020-12-05 23:28:48 +0100

    Update main.php

--- templates/italiapa/html/layouts/joomla/modal/main.php
+++ templates/italiapa/html/layouts/joomla/modal/main.php
@@ -47,118 +47,10 @@ extract($displayData);
 
 $modalClasses = array('Dialog', 'js-fr-dialogmodal');
 
-if (!isset($params['animation']) || $params['animation'])
-{
-	$modalClasses[] = 'fade';
-}
-
-$modalWidth = isset($params['modalWidth']) ? round((int) $params['modalWidth'], -1) : '';
-
-if ($modalWidth && $modalWidth > 0 && $modalWidth <= 100)
-{
-	$modalClasses[] = 'jviewport-width' . $modalWidth;
-}
-
 $modalAttributes = array(
 	'tabindex' => '-1',
 	'class'    => implode(' ', $modalClasses)
 );
-
-if (isset($params['backdrop']))
-{
-	$modalAttributes['data-backdrop'] = (is_bool($params['backdrop']) ? ($params['backdrop'] ? 'true' : 'false') : $params['backdrop']);
-}
-
-if (isset($params['keyboard']))
-{
-	$modalAttributes['data-keyboard'] = (is_bool($params['keyboard']) ? ($params['keyboard'] ? 'true' : 'false') : 'true');
-}
-
-/**
- * These lines below are for disabling scrolling of parent window.
- * $('body').addClass('modal-open');
- * $('body').removeClass('modal-open')
- *
- * Scrolling inside bootstrap modals on small screens (adapt to window viewport and avoid modal off screen).
- *      - max-height    .modal-body     Max-height for the modal body
- *                                      When height of the modal is too high for the window viewport height.
- *      - max-height    .iframe         Max-height for the iframe (Deducting the padding of the modal-body)
- *                                      When URL option is set and height of the iframe is higher than max-height of the modal body.
- *
- * Fix iOS scrolling inside bootstrap modals
- *      - overflow-y    .modal-body     When max-height is set for modal-body
- *
- * Specific hack for Bootstrap 2.3.x
- */
-$script[] = "jQuery(document).ready(function($) {";
-$script[] = "   $('#" . $selector . "').on('show.bs.modal', function() {";
-
-$script[] = "       $('body').addClass('modal-open');";
-
-if (isset($params['url']))
-{
-	$iframeHtml = JLayoutHelper::render('joomla.modal.iframe', $displayData);
-
-	// Script for destroying and reloading the iframe
-	$script[] = "       var modalBody = $(this).find('.modal-body');";
-	$script[] = "       modalBody.find('iframe').remove();";
-	$script[] = "       modalBody.prepend('" . trim($iframeHtml) . "');";
-}
-else
-{
-	// Set modalTooltip container to modal ID (selector), and placement to top-left if no data attribute (bootstrap-tooltip-extended.js)
-	$script[] = "       $('.modalTooltip').each(function(){;";
-	$script[] = "           var attr = $(this).attr('data-placement');";
-	$script[] = "           if ( attr === undefined || attr === false ) $(this).attr('data-placement', 'auto-dir top-left')";
-	$script[] = "       });";
-	$script[] = "       $('.modalTooltip').tooltip({'html': true, 'container': '#" . $selector . "'});";
-}
-
-// Adapt modal body max-height to window viewport if needed, when the modal has been made visible to the user.
-$script[] = "   }).on('shown.bs.modal', function() {";
-
-// Get height of the modal elements.
-$script[] = "       var modalHeight = $('div.modal:visible').outerHeight(true),";
-$script[] = "           modalHeaderHeight = $('div.modal-header:visible').outerHeight(true),";
-$script[] = "           modalBodyHeightOuter = $('div.modal-body:visible').outerHeight(true),";
-$script[] = "           modalBodyHeight = $('div.modal-body:visible').height(),";
-$script[] = "           modalFooterHeight = $('div.modal-footer:visible').outerHeight(true),";
-
-// Get padding top (jQuery position().top not working on iOS devices and webkit browsers, so use of Javascript instead)
-$script[] = "           padding = document.getElementById('" . $selector . "').offsetTop,";
-
-// Calculate max-height of the modal, adapted to window viewport height.
-$script[] = "           maxModalHeight = ($(window).height()-(padding*2)),";
-
-// Calculate max-height for modal-body.
-$script[] = "           modalBodyPadding = (modalBodyHeightOuter-modalBodyHeight),";
-$script[] = "           maxModalBodyHeight = maxModalHeight-(modalHeaderHeight+modalFooterHeight+modalBodyPadding);";
-
-if (isset($params['url']))
-{
-	// Set max-height for iframe if needed, to adapt to viewport height.
-	$script[] = "       var iframeHeight = $('.iframe').height();";
-	$script[] = "       if (iframeHeight > maxModalBodyHeight){;";
-	$script[] = "           $('.modal-body').css({'max-height': maxModalBodyHeight, 'overflow-y': 'auto'});";
-	$script[] = "           $('.iframe').css('max-height', maxModalBodyHeight-modalBodyPadding);";
-	$script[] = "       }";
-}
-else
-{
-	// Set max-height for modal-body if needed, to adapt to viewport height.
-	$script[] = "       if (modalHeight > maxModalHeight){;";
-	$script[] = "           $('.modal-body').css({'max-height': maxModalBodyHeight, 'overflow-y': 'auto'});";
-	$script[] = "       }";
-}
-
-$script[] = "   }).on('hide.bs.modal', function () {";
-$script[] = "       $('body').removeClass('modal-open');";
-$script[] = "       $('.modal-body').css({'max-height': 'initial', 'overflow-y': 'initial'});";
-$script[] = "       $('.modalTooltip').tooltip('destroy');";
-$script[] = "   });";
-$script[] = "});";
-
-JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 ?>
 <div id="<?php echo $selector; ?>" <?php echo ArrayHelper::toString($modalAttributes); ?>>
 	<div class="Dialog-content Dialog-content--centered u-background-white u-layout-prose u-margin-all-xl u-padding-all-xl js-fr-dialogmodal-modal" aria-labelledby="modal-title">
@@ -182,28 +74,3 @@ JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 		</div>
 	</div>
 </div>
-
-
-<!--
-<div class="Dialog js-fr-dialogmodal" id="modal">
-    <div class="
-      Dialog-content
-      Dialog-content--centered
-      u-background-white
-      u-layout-prose
-      u-margin-all-xl
-      u-padding-all-xl
-      js-fr-dialogmodal-modal
-    " aria-labelledby="modal-title">
-        <div role="document" class="Prose">
-            <h2 class="u-cf u-text-h2 u-borderHideFocus" id="modal-title" tabindex="0">A architecto qui et veritatis.</h2>
-            <p>
-                Illo voluptates debitis omnis necessitatibus quaerat. Pariatur architecto cumque. Delectus eius fuga. Sequi voluptatibus voluptatum nisi rem aut. Amet perferendis est vitae est facere consequatur officiis praesentium. Id quas ut reprehenderit provident
-                porro qui odio delectus. Et odit atque doloribus alias aut facere pariatur. In corporis aspernatur aliquam et quibusdam id natus. Voluptatem eius aut vel vel aliquam sit. Voluptatem autem labore aut debitis rem quae delectus maxime consequuntur.
-                Voluptate et labore ratione.
-            </p>
-            <button class="Button Button--danger js-fr-dialogmodal-close u-floatRight">Chiudi</button>
-        </div>
-    </div>
-</div>
--->
