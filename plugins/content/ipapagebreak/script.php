56e4c22ea82428fac7a18c2fa93a1bd17d2eb482
Helios Ciancio <eshiol@users.noreply.github.com>
2020-12-16 00:18:57 +0100

    copyright

--- plugins/content/ipapagebreak/script.php
+++ plugins/content/ipapagebreak/script.php
@@ -1,19 +1,20 @@
 <?php
 /**
- * @package		Joomla.Plugins
- * @subpackage	Content.Ipapagebreak
+ * @package     Joomla.Plugins
+ * @subpackage  Content.IpaPageBreak
  *
- * @version		__DEPLOY_VERSION__
+ * @version     __DEPLOY_VERSION__
  *
- * @author		Helios Ciancio <info@eshiol.it>
- * @link		http://www.eshiol.it
- * @copyright   Copyright (C) 2017 - 2020 Helios Ciancio. All rights reserved.
- * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
- * Template ItaliaPA is free software. This version may have been modified
- * pursuant to the GNU General Public License, and as distributed it includes
- * or or is derivative of works licensed under the GNU General Public License or or
- * other free or open source software licenses.
+ * @author      Helios Ciancio <info (at) eshiol (dot) it>
+ * @link        http://www.eshiol.it
+ * @copyright   Copyright (C) 2017 - 2020 Helios Ciancio. All Rights Reserved
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Joomla.Plugins.Content.Ipapagebreak  is  a  free  software. This version may
+ * have  been  modified  pursuant  to  the  GNU  General Public License, and as
+ * distributed  it  includes  or  is derivative of works licensed under the GNU 
+ * General Public License or other free or open source software licenses.
  */
+
 defined('_JEXEC') or die();
 
 use Joomla\CMS\Factory;
--- plugins/system/ipatinymce/ipatinymce.php
+++ plugins/system/ipatinymce/ipatinymce.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * @package		Joomla.Plugins
- * @subpackage	System.IpaTinyMCE
+ * @package     Joomla.Plugins
+ * @subpackage  System.IpaTinyMCE
  *
- * @version		__DEPLOY_VERSION__
+ * @version     __DEPLOY_VERSION__
  *
- * @author		Helios Ciancio <info@eshiol.it>
- * @link		http://www.eshiol.it
- * @copyright	Copyright (C) 2017 - 2020 Helios Ciancio. All Rights Reserved
- * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
- * Template ItaliaPA is free software. This version may have been modified
- * pursuant to the GNU General Public License, and as distributed it includes
- * or or is derivative of works licensed under the GNU General Public License or or
- * other free or open source software licenses.
+ * @author      Helios Ciancio <info (at) eshiol (dot) it>
+ * @link        http://www.eshiol.it
+ * @copyright   Copyright (C) 2017 - 2020 Helios Ciancio. All Rights Reserved
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Joomla.Plugins.System.IpaTinyMCE  is  a free software. This version may have
+ * been modified pursuant to the GNU General Public License, and as distributed
+ * it  includes or is derivative of works licensed under the GNU General Public
+ * License or other free or open source software licenses.
  */
 
 // no direct access
