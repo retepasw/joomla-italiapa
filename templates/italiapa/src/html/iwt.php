205a7a143263c4edc5efc044a928106fbade7035
Helios Ciancio <eshiol@users.noreply.github.com>
2020-12-27 15:46:42 +0100

    megamenu

--- templates/italiapa/src/html/iwt.php
+++ templates/italiapa/src/html/iwt.php
@@ -324,10 +324,6 @@ abstract class JHtmlIwt
 
 			$link .= '<span class="' . trim($item->menu_title_css . ($item->params->get('menu_text', 1) ? ' image-title' : ' u-hiddenVisually')) . '">' . $item->title . '</span>';
 		}
-		elseif (! $item->menu_image_css)
-		{
-			$link = $item->title;
-		}
 		elseif (preg_match_all('/(^|\s)Icon-/', $item->menu_image_css, $matches, PREG_SET_ORDER, 0) || preg_match_all('/(^|\s)it-/', $item->menu_image_css, $matches, PREG_SET_ORDER, 0))
 		{
 			$icon = '';
@@ -374,27 +370,10 @@ abstract class JHtmlIwt
 
 			$link .= '<span class="' . $item->menu_title_css . ($item->params->get('menu_text', 1) ? ' image-title' : ' u-hiddenVisually') . '">' . $item->title . '</span>';
 		}
-		elseif (preg_match_all('/(^|\s)icon-/', $item->menu_image_css, $matches, PREG_SET_ORDER, 0))
+		elseif ($item->menu_image_css)
 		{
-			$icon = '';
-			$menu_image_css = explode(' ', $item->menu_image_css);
-			for ($i = count($menu_image_css) - 1; $i >= 0; $i --)
-			{
-				if (substr($menu_image_css[$i], 0, 5) == 'icon-')
-				{
-					$icon .= ' ' . $menu_image_css[$i];
-					unset($menu_image_css[$i]);
-				}
-			}
-			$item->menu_image_css = implode(' ', $menu_image_css);
-
-			$class = trim($icon . ' ' . $item->menu_image_css);
-			if ($icon)
-			{
-				$link = '<span class="' . $class . '" data-icon></span>';
-			}
-
-			$link .= '<span class="' . $item->menu_title_css . ($item->params->get('menu_text', 1) ? ' image-title' : ' u-hiddenVisually') . '">' . $item->title . '</span>';
+            $link = '<span class="' . $item->menu_image_css . '"></span>';
+            $link .= '<span class="' . $item->menu_title_css . ($item->params->get('menu_text', 1) ? ' image-title' : ' u-hiddenVisually') . '">' . $item->title . '</span>';
 		}
 		elseif ($item->menu_title_css)
 		{
