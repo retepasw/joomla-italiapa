473a317141efd170fb66d22b4c5bdb886d397d64
Helios Ciancio <eshiol@users.noreply.github.com>
2021-01-25 19:33:55 +0100

    dropdown

---
+++ templates/italiapa/html/mod_articles_news/dropdown.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.ItaliaPA
+ *
+ * @version     __DEPLOY_VERSION__
+ *
+ * @author      Helios Ciancio <info (at) eshiol (dot) it>
+ * @link        https://www.eshiol.it
+ * @copyright   Copyright (C) 2017 - 2021 Helios Ciancio. All rights reserved
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
+ * Template ItaliaPA is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+defined('_JEXEC') or die;
+
+if (!empty($list)) :
+	$params->def('moduleclass_sfx', 'newsflash' . $moduleclass_sfx);
+	echo JLayoutHelper::render('eshiol.module.dropdown', array(
+		'module' => $module,
+		'list' => $list,
+		'params' => $params));
+endif;
+
