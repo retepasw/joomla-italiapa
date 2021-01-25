4cb5af86acd5d8dfc2a3f43aa220b58842677c00
Helios Ciancio <eshiol@users.noreply.github.com>
2021-01-25 19:31:21 +0100

    dropdown

--- templates/italiapa/html/mod_articles_latest/dropdown.php
+++ templates/italiapa/html/mod_articles_latest/dropdown.php
@@ -16,30 +16,11 @@
  */
 
 defined('_JEXEC') or die;
-?>
-<?php if (!empty($list)) : ?>
-<div class="latestnews<?php echo $moduleclass_sfx; ?> mod-list">
-	<a href="#module_<?php echo $module->id; ?>" data-menu-trigger="module_<?php echo $module->id; ?>" class="Header-language u-border-none u-zindex-max u-inlineBlock" aria-controls="module_<?php echo $module->id; ?>" aria-haspopup="true" role="button">
-		<?php if ($module->showtitle) : ?>
-			<?php echo $module->title; ?>
-		<?php endif; ?>
-		<span class="Icon Icon-expand u-padding-left-xs"></span>
-	</a>
-	<div id="module_<?php echo $module->id; ?>" data-menu="" class="Dropdown-menu Header-language-other u-jsVisibilityHidden u-nojsDisplayNone" x-placement="bottom" role="menu" aria-hidden="true" style="position: absolute; transform: translate3d(1323px, -245px, 0px); top: 0px; left: 0px; will-change: transform;">
-		<span class="Icon-drop-down Dropdown-arrow u-color-white" style="left: 58px;"></span>
-		<ul class="latestnews<?php echo $moduleclass_sfx; ?> mod-list" itemscope itemtype="http://schema.org/ItemList">
-			<?php $i = 1; ?>
-			<?php foreach ($list as $item) : ?>
-				<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-					<meta itemprop="position" content="<?php echo $i++; ?>"/>
-					<a href="<?php echo $item->link; ?>" itemprop="url">
-						<span itemprop="name">
-							<?php echo $item->title; ?>
-						</span>
-					</a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-</div>
-<?php endif;
+
+if (!empty($list)) :
+	$params->def('moduleclass_sfx', 'latestnews' . $moduleclass_sfx);
+	echo JLayoutHelper::render('eshiol.module.dropdown', array(
+		'module' => $module, 
+		'list' => $list, 
+		'params' => $params));
+endif;
