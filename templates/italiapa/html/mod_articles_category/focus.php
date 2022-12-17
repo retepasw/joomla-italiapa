0a87fc7d97564a94270b8cfe7e3a63a71813d43a
Helios Ciancio <12718836+eshiol@users.noreply.github.com>
2022-12-17 11:36:08 +0100

    fix Undefined variable $icon

--- templates/italiapa/html/mod_articles_category/focus.php
+++ templates/italiapa/html/mod_articles_category/focus.php
@@ -59,8 +59,8 @@ $moduleclass_sfx = (substr($params->get('moduleclass_sfx'), 0, 1) == ' ' ? ' ' :
 						<?php endif; ?>
 
 						<h3 class="u-text-h4 u-margin-r-bottom">
+							<?php $icon = ''; ?>
 							<?php if (JPluginHelper::getPlugin('system', 'fields')) : ?>
-								<?php $icon = ''; ?>
 								<?php $jcFields = FieldsHelper::getFields('com_content.article', $item, true); ?>
 								<?php foreach ($jcFields as $jcField) : ?>
 									<?php if (($jcField->name == 'articleicon') && $jcField->rawvalue): ?>
