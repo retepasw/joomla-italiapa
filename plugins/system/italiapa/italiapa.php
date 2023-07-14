ffb8cda1ba12d8ffee868e18176dbec8e701fd38
Helios Ciancio <12718836+eshiol@users.noreply.github.com>
2023-07-14 10:56:20 +0200

    feat: redirect after save profile

--- plugins/system/italiapa/italiapa.php
+++ plugins/system/italiapa/italiapa.php
@@ -17,7 +17,16 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Associations;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
+use Joomla\Utilities\ArrayHelper;
 
 /**
  * Joomla! ItaliaPA Plugin.
@@ -177,6 +186,181 @@ class PlgSystemItaliaPA extends JPlugin
 			\JLoader::registerPrefix('J', __DIR__ . '/src/joomla3/cms', false, true);
 			
 			$loader->addClassMap($classMap);
+		}
+
+		+
+		$userId = Factory::getUser()->id;
+
+		// Run this in frontend only
+		if ($this->app->isClient('site'))
+		{
+
+			// Check to see whether user already consented, if not, redirect to user profile page
+			if ($userId > 0)
+			{
+				// If user consented before, no need to check it further
+				if (!$this->isUserConsented($userId))
+				{
+					$option = $this->app->input->getCmd('option');
+					$task   = $this->app->input->get('task');
+					$view   = $this->app->input->getString('view', '');
+					$layout = $this->app->input->getString('layout', '');
+					$id     = $this->app->input->getInt('id');
+
+					$privacyArticleId = $this->getPrivacyArticleId();
+
+					/*
+					* If user is already on edit profile screen or view privacy article
+					* or press update/apply button, or logout, do nothing to avoid infinite redirect
+					*/
+					if ($option == 'com_users' && in_array($task, array('profile.save', 'profile.apply', 'user.logout', 'user.menulogout'))
+						|| ($option == 'com_content' && $view == 'article' && $id == $privacyArticleId)
+						|| ($option == 'com_users' && $view == 'profile' && $layout == 'edit'))
+					{
+						return;
+					}
+
+					Factory::getApplication()->setUserState('com_users.edit.profile.redirect', base64_decode($this->app->input->get('return', base64_encode(Uri::getInstance()->toString()), 'base64')));
+				}
+			}
+		}
+		else
+		{
+			if ($userId > 0)
+			{
+				if (PluginHelper::isEnabled('system', 'privacyconsent'))
+				{
+					$query = $this->db->getQuery(true);
+					$query->select($this->db->quoteName('name'))
+						->from('#__extensions')
+						->where($this->db->quoteName('type') . ' = ' . $this->db->quote('plugin'))
+						->where($this->db->quoteName('folder') . ' = ' . $this->db->quote('system'))
+						->where($this->db->quoteName('element') . ' IN (' . $this->db->quote('italiapa') . ', ' . $this->db->quote('privacyconsent') . ')')
+						->order('ordering');
+					$query->setLimit(1);
+					$this->db->setQuery($query);
+		
+					$name = $this->db->loadResult();
+
+					if ($name != 'plg_system_italiapa')
+					{
+						$this->app->enqueueMessage('Il plugin System - ItaliaPA deve essere posizionato prima del plugin System - Privacy Content');
+					}
+				}
+				if (PluginHelper::isEnabled('system', 'languagefilter'))
+				{
+					$plugin = PluginHelper::getPlugin('system', 'languagefilter');
+					$params = new Registry($plugin->params);
+					if ($params->get('automatic_change') == 1)
+					{
+						// $this->app->enqueueMessage('Il plugin System - Language Filter deve avere l\'opzione Cambio lingua automatico disabilitata');
+						$query = $this->db->getQuery(true);
+						$query->select($this->db->quoteName('name'))
+							->from('#__extensions')
+							->where($this->db->quoteName('type') . ' = ' . $this->db->quote('plugin'))
+							->where($this->db->quoteName('folder') . ' = ' . $this->db->quote('system'))
+							->where($this->db->quoteName('element') . ' IN (' . $this->db->quote('italiapa') . ', ' . $this->db->quote('languagefilter') . ')')
+							->order('ordering');
+						$query->setLimit(1);
+						$this->db->setQuery($query);
+			
+						$name = $this->db->loadResult();
+
+						if ($name != 'plg_system_languagefilter')
+						{
+							$this->app->enqueueMessage('Il plugin System - ItaliaPA deve essere posizionato dopo il plugin System - Language Filter');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method to check if the given user has consented yet
+	 *
+	 * @param   integer  $userId  ID of uer to check
+	 *
+	 * @return  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function isUserConsented($userId)
+	{
+		if (PluginHelper::isEnabled('system', 'privacyconsent'))
+		{
+			$query = $this->db->getQuery(true);
+			$query->select('COUNT(*)')
+				->from('#__privacy_consents')
+				->where('user_id = ' . (int) $userId)
+				->where('subject = ' . $this->db->quote('PLG_SYSTEM_PRIVACYCONSENT_SUBJECT'))
+				->where('state = 1');
+			$this->db->setQuery($query);
+
+			return (int) $this->db->loadResult() > 0;
+		}
+		else
+		{	
+			return true;
+		}
+	}
+
+	/**
+	 * Get privacy article ID. If the site is a multilingual website and there is associated article for the
+	 * current language, ID of the associated article will be returned
+	 *
+	 * @return  integer
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function getPrivacyArticleId()
+	{
+		$plugin = PluginHelper::getPlugin('system', 'privacyconsent');
+		$params = new Registry($plugin->params);
+
+		$privacyArticleId = $params->get('privacy_article');
+
+		if ($privacyArticleId > 0 && Associations::isEnabled())
+		{
+			$privacyAssociated = Associations::getAssociations('com_content', '#__content', 'com_content.item', $privacyArticleId);
+			$currentLang = Factory::getLanguage()->getTag();
+
+			if (isset($privacyAssociated[$currentLang]))
+			{
+				$privacyArticleId = $privacyAssociated[$currentLang]->id;
+			}
+		}
+
+		return $privacyArticleId;
+	}
+
+
+	/**
+	 * After store user method.
+	 *
+	 * Method is called after user data is stored in the database.
+	 *
+	 * @param   array    $user     Holds the new user data.
+	 * @param   boolean  $isnew    True if a new user is stored.
+	 * @param   boolean  $success  True if user was succesfully stored in the database.
+	 * @param   string   $msg      Message.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onUserAfterSave($user, $isnew, $success, $msg)
+	{
+		if (empty(Factory::getApplication()->getUserState('com_users.edit.profile.redirect')))
+		{
+			if ($return = $this->app->input->get('return', null, 'base64'))
+			{
+				Factory::getApplication()->setUserState('com_users.edit.profile.redirect', base64_decode($return));
+			}
+			elseif ($return = $this->app->input->post->get('return', null, 'base64'))
+			{
+				Factory::getApplication()->setUserState('com_users.edit.profile.redirect', base64_decode($return));
+			}
 		}
 	}
 }
